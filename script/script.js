73e65be80c1ad18986bd5ea10a615d36fe4f0da3
Ilya <volkov.ilyya@gmail.com>
2023-05-01 13:57:17 +0200

    added html page and test script

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const greenButton = document.getElementById("green_frame");
+const moveButton = document.getElementById("diagonal_move");
+
+greenButton.addEventListener("click", () => {
+  const url = "http://127.0.0.1:17000";
+});
