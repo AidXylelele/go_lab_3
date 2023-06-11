abf9b1cc54f1801b7ea209ac5e3d3f6201706514
AIDXYLELELE <warboyka48@gmail.com>
2023-06-11 15:32:03 +0200

    fix: Moved the UI state to a separate component

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -11,7 +11,7 @@ import (
 	"github.com/AidXylelele/go_lab_3/painter"
 )
 
-type Parser struct {
+type UIState struct {
 	lastBgColor painter.PainterOperation
 	lastBgRect  *painter.BgRectangle
 	figures     []*painter.Figure
@@ -19,12 +19,16 @@ type Parser struct {
 	updateOp    painter.PainterOperation
 }
 
+type Parser struct {
+	uiState UIState
+}
+
 func (p *Parser) initialize() {
-	if p.lastBgColor == nil {
-		p.lastBgColor = painter.OperationFunc(painter.ResetScreen)
+	if p.uiState.lastBgColor == nil {
+		p.uiState.lastBgColor = painter.OperationFunc(painter.ResetScreen)
 	}
-	if p.updateOp != nil {
-		p.updateOp = nil
+	if p.uiState.updateOp != nil {
+		p.uiState.updateOp = nil
 	}
 }
 
@@ -46,34 +50,34 @@ func (p *Parser) Parse(in io.Reader) ([]painter.PainterOperation, error) {
 
 func (p *Parser) finalResult() []painter.PainterOperation {
 	var res []painter.PainterOperation
-	if p.lastBgColor != nil {
-		res = append(res, p.lastBgColor)
+	if p.uiState.lastBgColor != nil {
+		res = append(res, p.uiState.lastBgColor)
 	}
-	if p.lastBgRect != nil {
-		res = append(res, p.lastBgRect)
+	if p.uiState.lastBgRect != nil {
+		res = append(res, p.uiState.lastBgRect)
 	}
-	if len(p.moveOps) != 0 {
-		res = append(res, p.moveOps...)
+	if len(p.uiState.moveOps) != 0 {
+		res = append(res, p.uiState.moveOps...)
 	}
-	p.moveOps = nil
-	if len(p.figures) != 0 {
-		println(len(p.figures))
-		for _, figure := range p.figures {
+	p.uiState.moveOps = nil
+	if len(p.uiState.figures) != 0 {
+		println(len(p.uiState.figures))
+		for _, figure := range p.uiState.figures {
 			res = append(res, figure)
 		}
 	}
-	if p.updateOp != nil {
-		res = append(res, p.updateOp)
+	if p.uiState.updateOp != nil {
+		res = append(res, p.uiState.updateOp)
 	}
 	return res
 }
 
 func (p *Parser) resetState() {
-	p.lastBgColor = nil
-	p.lastBgRect = nil
-	p.figures = nil
-	p.moveOps = nil
-	p.updateOp = nil
+	p.uiState.lastBgColor = nil
+	p.uiState.lastBgRect = nil
+	p.uiState.figures = nil
+	p.uiState.moveOps = nil
+	p.uiState.updateOp = nil
 }
 
 func (p *Parser) parse(commandStr string) error {
@@ -99,32 +103,32 @@ func (p *Parser) parse(commandStr string) error {
 
 		switch instruction {
 		case "white":
-			p.lastBgColor = painter.OperationFunc(painter.WhiteFill)
+			p.uiState.lastBgColor = painter.OperationFunc(painter.WhiteFill)
 		case "green":
-			p.lastBgColor = painter.OperationFunc(painter.GreenFill)
+			p.uiState.lastBgColor = painter.OperationFunc(painter.GreenFill)
 		case "bgrect":
 			if len(iArgs) != 4 {
 				return fmt.Errorf("invalid number of arguments for command bgrect: %v", len(iArgs))
 			}
-			p.lastBgRect = &painter.BgRectangle{X1: iArgs[0], Y1: iArgs[1], X2: iArgs[2], Y2: iArgs[3]}
+			p.uiState.lastBgRect = &painter.BgRectangle{X1: iArgs[0], Y1: iArgs[1], X2: iArgs[2], Y2: iArgs[3]}
 		case "figure":
 			if len(iArgs) != 2 {
 				return fmt.Errorf("invalid number of arguments for command figure: %v", len(iArgs))
 			}
 			clr := color.RGBA{R: 255, G: 255, B: 0, A: 1}
 			figure := painter.Figure{X: iArgs[0], Y: iArgs[1], C: clr}
-			p.figures = append(p.figures, &figure)
+			p.uiState.figures = append(p.uiState.figures, &figure)
 		case "move":
 			if len(iArgs) != 2 {
 				return fmt.Errorf("invalid number of arguments for command move: %v", len(iArgs))
 			}
-			moveOp := painter.Move{X: iArgs[0], Y: iArgs[1], Figures: p.figures}
-			p.moveOps = append(p.moveOps, &moveOp)
+			moveOp := painter.Move{X: iArgs[0], Y: iArgs[1], Figures: p.uiState.figures}
+			p.uiState.moveOps = append(p.uiState.moveOps, &moveOp)
 		case "reset":
 			p.resetState()
-			p.lastBgColor = painter.OperationFunc(painter.ResetScreen)
+			p.uiState.lastBgColor = painter.OperationFunc(painter.ResetScreen)
 		case "update":
-			p.updateOp = painter.UpdateOperation
+			p.uiState.updateOp = painter.UpdateOperation
 		default:
 			return fmt.Errorf("unknown command: %v", instruction)
 		}
